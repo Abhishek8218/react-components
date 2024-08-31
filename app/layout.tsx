ebb4155c9af60ccdc349f96c1c1639bf67ca56c4
Abhishek Kumar <ak3037308@gmail.com>
2024-08-31 15:36:50 +0530

    chore: Add QueryProvider to RootLayout for managing queries

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { TourProvider } from "@/context/tourContext";
+import QueryProvider from "@/provider/QueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <QueryProvider>
         <TourProvider>
 
         {children}
 
         </TourProvider>
+        </QueryProvider>
         
         </body>
     </html>
